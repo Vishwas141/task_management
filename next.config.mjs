7ce2087f1da60e82a6d1891a38afbda2784bfcf5
vishwas <nalawadevishwas14@gmail.com>
2024-09-21 22:54:29 +0530

    suspense added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@hello-pangea/dnd'],
+    experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
